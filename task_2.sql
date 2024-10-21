be03909726af95f70f7c1eecfcd84546bdc828d4
Mina Markos <minalxr123@gmail.com>
2024-10-21 05:01:58 +0300

    work in Task_1

--- task_2.sql
+++ task_2.sql
@@ -29,3 +29,11 @@ CREATE TABLE Orders (
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
+
+
+CREATE TABLE Order_Details (
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+      
+);
